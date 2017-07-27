3534f575c99ff44ddc85ba26c63c8190b3fb7604
Kassio Machado <kassio.comp@gmail.com>
2017-07-26 22:05:41 -0400

    adding the command line feature to crawler and an example of config file

--- WeatherUndergroundCrawler.py
+++ WeatherUndergroundCrawler.py
@@ -6,6 +6,7 @@
 # OUTPUT: all JSON received in response of HISTORY queries
 # the fields of query are describe on http://www.wunderground.com/weather/api/d/docs?d=data/history
 
+import sys
 import urllib2
 import json
 import time
@@ -14,49 +15,52 @@ import datetime
 import colorama
 from ConfigParser import SafeConfigParser
 
-DATE_FORMAT = '%Y-%m-%d'
+if __name__ == "__main__":
 
-parser = SafeConfigParser()				# initalizing the parser
-parser.read('WeatherUnderground.cfg')				# loading config file
-country = parser.get('uweather', 'country') 	# loading the file with section and the propertie name
-city = parser.get('uweather', 'city')
-dateBegin = datetime.datetime.strptime(parser.get('uweather', 'dateBegin'), DATE_FORMAT)	# dateBegin = datetime.date(2014,9,1)
-dateEnd = datetime.datetime.strptime(parser.get('uweather', 'dateEnd'), DATE_FORMAT)		# dateEnd = datetime.date(2014,12,31)
-keyindex = parser.get('uweather', 'keyindex')
-keyAuth = parser.get('uweather', 'keyAuth').split(',')[int(keyindex)]
+	DATE_FORMAT = '%Y-%m-%d'
 
-filename = 'uweather-'+city.lower()+'-'+str(dateBegin.year)+'.json'
-outputFile = open(filename, 'a', 0)
-print colorama.Fore.WHITE + colorama.Back.RED + 'Weather Underground Data Crawler - PhD. Kassio Machado (Candidate)\nCity ' + city + '\n' + 'keyAuth ' + keyAuth \
-+ '\nPeriod ' + dateBegin.strftime('%Y-%m-%d') + ' to ' + dateEnd.strftime('%Y-%m-%d') + '\nFile ' + filename + ' (a)' +colorama.Fore.RESET + colorama.Back.RESET
+	configFilename = sys.argv[1]
+	parser = SafeConfigParser()				# initalizing the parser
+	parser.read(configFilename)				# loading config file
+	country = parser.get('uweather', 'country') 	# loading the file with section and the propertie name
+	city = parser.get('uweather', 'city')
+	dateBegin = datetime.datetime.strptime(parser.get('uweather', 'dateBegin'), DATE_FORMAT)	# dateBegin = datetime.date(2014,9,1)
+	dateEnd = datetime.datetime.strptime(parser.get('uweather', 'dateEnd'), DATE_FORMAT)		# dateEnd = datetime.date(2014,12,31)
+	keyindex = parser.get('uweather', 'keyindex')
+	keyAuth = parser.get('uweather', 'keyAuth').split(',')[int(keyindex)]
 
-while True:
-	try:
-		dateRec = dateBegin.strftime('%Y%m%d')
-		if dateBegin.weekday() >= 5:
-			colorDate = colorama.Fore.RED
-		else:
-			colorDate = colorama.Fore.YELLOW
-		url = 'URL: http://api.wunderground.com/api/'+keyAuth+'/history_'+dateRec+'/q/'+country+'/'+city+'.json'
-		print url
-		f = urllib2.urlopen(url)
-		json_string = f.read()
-		parsed_json = json.loads(json_string)
-		dateStr = parsed_json['history']['date']['pretty']
-		json.dump(parsed_json, outputFile)
-		outputFile.write('\n')
-		f.close()
-		print colorDate+'Weather Data: ' + country + '-' + city + ' ' + dateStr+' (wait 7 seconds)'+colorama.Fore.RESET
-		time.sleep(7)
-		if dateBegin == dateEnd:
-			break
-		else:
-			dateBegin += datetime.timedelta(days=1)
-	except Exception, e:
-		print 'Error: ' + str(e)
-		time.sleep(60)
+	filename = 'uweather-'+city.lower()+'-'+str(dateBegin.year)+'.json'
+	outputFile = open(filename, 'a', 0)
+	print colorama.Fore.WHITE + colorama.Back.RED + 'Weather Underground Data Crawler - PhD. Kassio Machado (Candidate)\nCity ' + city + '\n' + 'keyAuth ' + keyAuth \
+	+ '\nPeriod ' + dateBegin.strftime('%Y-%m-%d') + ' to ' + dateEnd.strftime('%Y-%m-%d') + '\nFile ' + filename + ' (a)' +colorama.Fore.RESET + colorama.Back.RESET
 
-outputFile.close()
+	while True:
+		try:
+			dateRec = dateBegin.strftime('%Y%m%d')
+			if dateBegin.weekday() >= 5:
+				colorDate = colorama.Fore.RED
+			else:
+				colorDate = colorama.Fore.YELLOW
+			url = 'URL: http://api.wunderground.com/api/'+keyAuth+'/history_'+dateRec+'/q/'+country+'/'+city+'.json'
+			print url
+			f = urllib2.urlopen(url)
+			json_string = f.read()
+			parsed_json = json.loads(json_string)
+			dateStr = parsed_json['history']['date']['pretty']
+			json.dump(parsed_json, outputFile)
+			outputFile.write('\n')
+			f.close()
+			print colorDate+'Weather Data: ' + country + '-' + city + ' ' + dateStr+' (wait 7 seconds)'+colorama.Fore.RESET
+			time.sleep(7)
+			if dateBegin == dateEnd:
+				break
+			else:
+				dateBegin += datetime.timedelta(days=1)
+		except Exception, e:
+			print 'Error: ' + str(e)
+			time.sleep(60)
+
+	outputFile.close()
 
 # dictCities = dict()
 # dictCities['chicago'] = ('IL', 'Chicago')
