6be56b771241611635bccc6d1cea720cb1de98f1
Kassio Machado <kassio.comp@gmail.com>
2017-07-28 15:00:39 -0400

    interface CLI

--- TwitterMonitorTools.py
+++ TwitterMonitorTools.py
@@ -11,12 +11,13 @@ import datetime
 from tqdm import tqdm
 import TwitterMonitor
 
-def exportPlaces(args):
-	""" Count the samples and group them according to the places indicated by
+def exportStatsCountryPlaces(inputfiles):
+	"""
+	Count the samples and group them according to the places indicated by
 	Twitter sample (not dependent of app - like Instagram of Foursquare). The
 	method exports two CSV files for each input file containing the places and
-	countries. """
-	inputfiles = [args] if type(args) != list else args
+	countries.
+	"""
 	for filename in inputfiles:
 		dataPlaces, dataCountries = TwitterMonitor.loadPlaceStats(filename)
 		outputfilename = filename.replace('.csv', '-countries.csv')
@@ -33,109 +34,11 @@ def exportPlaces(args):
 		for p in sorted(dataPlaces.keys(), key=lambda k:dataPlaces[k][4], reverse=True):
 			writer.writerow(dataPlaces[p])
 
-
-def exportPlaceURLByCountry(args):
-	""" Filters the fields and exports in CSV files the information related to the
-	samples' venues, such as place name, place type, country, Twitter URL of place,
-	and Instagram URL of the sample. """
-
-	isoCodeCountry = args[0]
-	inputfiles = args[1:]
-	print colorama.Fore.CYAN, 'Exporting files with URLs from', isoCodeCountry, colorama.Fore.RESET
-	for filename in inputfiles:
-		print colorama.Fore.RED, filename, colorama.Fore.RESET
-		inputfile = open(filename, 'r')
-		outputfile = open(filename.replace('.csv', '-url-' + isoCodeCountry.upper() + '.csv'), 'w')
-		lineBuffer = list()
-		invalidSample = 0
-		for line in tqdm(inputfile, desc='Collecting URL\'s'):
-			try:
-				sample = eval(line.replace('\n', ''))
-				country = sample['country'].upper()
-				if country != isoCodeCountry:
-					continue
-				id_data = sample['id_data']
-				id_user = sample['userid']
-				country = sample['country']
-				url = sample['urls'][-1]['expanded_url']
-				place_url = sample['place_url']
-				place_name = sample['place_name']
-				line = id_data + ',' + id_user + ',' + country
-				line += ',' + url + ',' + place_url
-				line += ',' + place_name.replace(',', ';') + '\n'
-				lineBuffer.append(line)
-				if lineBuffer >= 100000:
-					for l in lineBuffer:
-						outputfile.write(l)
-					lineBuffer = list()
-			except KeyError:
-				invalidSample += 1
-				continue
-			except SyntaxError:
-				invalidSample += 1
-				continue
-		for l in lineBuffer:
-			outputfile.write(l)
-
-def exportPlaceURLByBoundBox(args):
+def exportPlaceURL(inputfiles):
 	"""
-	Exports the files containing the URLs from samples locations according
-	to Instagram. The function requires the pre-defined bounding box on
-	TwitterMonitor.cfg.
+	Export samples' places info and URL
+	independently of city and country.
 	"""
-	locationName = args[0]
-	inputfiles = args[1:]
-	configparser = TwitterMonitor.loadConfigParser('TwitterMonitor.cfg')
-	coords = TwitterMonitor.loadBoundBox(configparser, locationName)
-	lng0, lngn = sorted([coords[0], coords[2]])
-	lat0, latn = sorted([coords[1], coords[3]])
-	print lat0, latn, lng0, lngn
-	print colorama.Fore.CYAN, 'Exporting files with URLs from', locationName, colorama.Fore.RESET
-	for filename in inputfiles:
-		print colorama.Fore.RED, filename, colorama.Fore.RESET
-		inputfile = open(filename, 'r')
-		outputfile = open(filename.replace('.csv', '-url-' + locationName.upper() + '.csv'), 'w')
-		lineBuffer = list()
-		invalidSample = 0
-		for line in tqdm(inputfile, desc='Exporting URL\'s', disable=False):
-			try:
-				sample = eval(line.replace('\n', ''))
-				lng = float(sample['lat'])
-				lat = float(sample['lng'])
-				if lat >= lat0 and lat <= latn and lng >= lng0 and lng <= lngn:
-					id_data = sample['id_data'].encode('utf-8')
-					id_user = sample['userid'].encode('utf-8')
-					country = sample['country'].encode('utf-8')
-					url = sample['urls'][-1]['expanded_url'].encode('utf-8')
-					place_url = sample['place_url'].encode('utf-8')
-					place_name = sample['place_name'].encode('utf-8')
-					date_local = sample['date_local']
-					line = id_data
-					line += ',' + id_user
-					line += ',' + country
-					line += ',' + url
-					line += ',' + place_url
-					line += ',' + place_name.replace(',', ';')
-					line += ',' + date_local.strftime('%y-%m-%d %H:%M:%S')
-					line += '\n'
-					lineBuffer.append(line)
-					if lineBuffer >= 100000:
-						for l in lineBuffer:
-							outputfile.write(l)
-						lineBuffer = list()
-			except KeyError:
-				invalidSample += 1
-				continue
-			except SyntaxError:
-				invalidSample += 1
-				continue
-		for l in lineBuffer:
-			outputfile.write(l)
-		print '#' + str(invalidSample),'Invalid Samples'
-
-def exportPlaceURL(args):
-	""" General - Samples independently of city/country """
-	inputfiles = args
 	for filename in inputfiles:
 		print colorama.Fore.RED, filename, colorama.Fore.RESET
 		inputfile = open(filename, 'r')
@@ -175,9 +78,119 @@ def exportPlaceURL(args):
 			outputfile.write(l)
 		print '#' + str(invalidSample),'Invalid Samples'
 
+def exportPlaceURLByCountry(isoCodeCountry, inputfiles):
+	"""
+	Filters the fields and exports in CSV files the information related to the
+	samples' venues, such as place name, place type, country, Twitter URL of place,
+	and Instagram URL of the sample, according to the country code indicade
+	according to the ISO Alpha-2
+	"""
+	print colorama.Fore.CYAN, 'Exporting files with URLs from', isoCodeCountry, colorama.Fore.RESET
+	for filename in inputfiles:
+		print colorama.Fore.RED, filename, colorama.Fore.RESET
+		inputfile = open(filename, 'r')
+		outputfile = open(filename.replace('.csv', '-url-' + isoCodeCountry.upper() + '.csv'), 'w')
+		lineBuffer = list()
+		invalidSample = 0
+		for line in tqdm(inputfile, desc='Collecting URL\'s'):
+			try:
+				sample = eval(line.replace('\n', ''))
+				country = sample['country'].upper()
+				if country != isoCodeCountry:
+					continue
+				id_data = sample['id_data']
+				id_user = sample['userid']
+				country = sample['country']
+				url = sample['urls'][-1]['expanded_url']
+				place_url = sample['place_url']
+				place_name = sample['place_name']
+				line = id_data + ',' + id_user + ',' + country
+				line += ',' + url + ',' + place_url
+				line += ',' + place_name.replace(',', ';') + '\n'
+				lineBuffer.append(line)
+				if lineBuffer >= 25000:
+					for l in lineBuffer:
+						outputfile.write(l)
+					lineBuffer = list()
+			except KeyError:
+				invalidSample += 1
+				continue
+			except SyntaxError:
+				invalidSample += 1
+				continue
+		for l in lineBuffer:
+			outputfile.write(l)
+
+def exportPlaceURLByBoundBox(locationName, inputfiles, configFilename='TwitterMonitor.cfg'):
+	"""
+	Exports the files containing the URLs from samples locations according
+	to Instagram. The function requires the pre-defined bounding box on
+	TwitterMonitor.cfg.
+	"""
+
+	configparser = TwitterMonitor.loadConfigParser(configFilename)
+	coords = TwitterMonitor.loadBoundBox(configparser, locationName)
+	lng0, lngn = sorted([coords[0], coords[2]])
+	lat0, latn = sorted([coords[1], coords[3]])
+	print lat0, latn, lng0, lngn
+	print colorama.Fore.CYAN, 'Exporting files with URLs from', locationName, colorama.Fore.RESET
+	for filename in inputfiles:
+		print colorama.Fore.RED, filename, colorama.Fore.RESET
+		inputfile = open(filename, 'r')
+		outputfile = open(filename.replace('.csv', '-url-' + locationName.upper() + '.csv'), 'w')
+		lineBuffer = list()
+		invalidSample = 0
+		for line in tqdm(inputfile, desc='Exporting URL\'s', disable=False):
+			try:
+				sample = eval(line.replace('\n', ''))
+				lng = float(sample['lat'])
+				lat = float(sample['lng'])
+				if lat >= lat0 and lat <= latn and lng >= lng0 and lng <= lngn:
+					id_data = sample['id_data'].encode('utf-8')
+					id_user = sample['userid'].encode('utf-8')
+					country = sample['country'].encode('utf-8')
+					url = sample['urls'][-1]['expanded_url'].encode('utf-8')
+					place_url = sample['place_url'].encode('utf-8')
+					place_name = sample['place_name'].encode('utf-8')
+					date_local = sample['date_local']
+					line = id_data
+					line += ',' + id_user
+					line += ',' + country
+					line += ',' + url
+					line += ',' + place_url
+					line += ',' + place_name.replace(',', ';')
+					line += ',' + date_local.strftime('%y-%m-%d %H:%M:%S')
+					line += '\n'
+					lineBuffer.append(line)
+					if lineBuffer >= 1000:
+						for l in lineBuffer:
+							outputfile.write(l)
+						lineBuffer = list()
+			except KeyError:
+				invalidSample += 1
+				continue
+			except SyntaxError:
+				invalidSample += 1
+				continue
+		for l in lineBuffer:
+			outputfile.write(l)
+		print '#' + str(invalidSample),'Invalid Samples'
+
 if __name__ == "__main__":
 	args = sys.argv[1:]
-	# TODO add CLI for methods
-	# exportPlaceURL(args)
-	# exportLocations(args)
-	exportPlaceURLByBoundBox(args)
+	f = args.pop(0)
+	if f == 'stats':
+		exportStatsCountryPlaces(args)
+	elif f == 'url':
+		inputfiles = args
+		exportPlaceURL(inputfiles)
+	elif f == 'url-country':
+		isoCodeCountry = args.pop(0)
+		inputfiles = args
+		exportPlaceURLByCountry(isoCodeCountry, inputfiles)
+	elif f == 'url-bbox':
+		locationName = args.pop(0)
+		inputfiles = args
+		exportPlaceURLByBoundBox(locationName, inputfiles)
+	else:
+		print 'look in the code to know the CLI haha :)'
