2b2851e8e8d4c7bfcc9a420380a816078a83eed0
Kassio Machado <leokassio@Kassios-MacBook-Pro.local>
2017-09-15 01:03:13 -0400

    small improvements

--- data-acquisition/FoursquarePlaces.py
+++ data-acquisition/FoursquarePlaces.py
@@ -4,6 +4,7 @@ import sys
 import json
 import colorama
 import requests
+import datetime
 from tqdm import tqdm
 
 INFO = colorama.Fore.BLACK + colorama.Back.GREEN + '[INFO] '
@@ -11,12 +12,24 @@ WARNING = colorama.Fore.BLACK + colorama.Back.YELLOW + '[WRNG] '
 ERROR = colorama.Fore.BLACK + colorama.Back.RED + '[ERROR] '
 RESET = colorama.Fore.RESET + colorama.Back.RESET
 
-url = 'https://api.foursquare.com/v2/venues/search?'
-url += 'oauth_token=VKE2KCEVOAHRFRTEFR15I2V5KLBRYWUKF0LEK2YVMMXJKZQP'
-url += '&v=20131016&ll=40.7142%2C-74.0064'
-url += '&query=New%20York%2C%20New%20York'
-url += '&intent=checkin'
-resp = requests.get(url)
+url = 'https://api.foursquare.com/v2/venues/search'
+payload = dict()
+payload['ll'] = '41.88256075,-87.623115'
+payload['client_id'] = ''
+payload['client_secret'] = ''
+payload['v'] = '20160616' #datetime.datetime.now().strftime('%Y%m%d')
+payload['query'] = 'The Giant'
+payload['intent'] = 'match'
+resp = requests.get(url, params=payload)
 
-rate = int(r.headers['X-RateLimit-Remaining'])
-data = json.loads(resp.content)
+print WARNING + resp.url + RESET
+
+try:
+    rate = int(resp.headers['X-RateLimit-Remaining'])
+    print rate, 'reqs reamining'
+    data = json.loads(resp.content)
+    outputfile = open('response.json', 'w', 0)
+    json.dump(data, outputfile, indent=4)
+except KeyError:
+    print resp.headers
+    print resp.content
