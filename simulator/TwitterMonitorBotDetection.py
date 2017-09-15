c07b9c16fba7d9e3fa2c87845d27e756e9479bfd
Kassio Machado <leokassio@Kassios-MacBook-Pro.local>
2017-09-15 01:02:44 -0400

    New CLI using parse and exception handlement

--- simulator/TwitterMonitorBotDetection.py
+++ simulator/TwitterMonitorBotDetection.py
@@ -3,10 +3,12 @@
 import csv
 import sys
 import json
+import argparse
 import colorama
 import requests
 import datetime
 import botometer # https://github.com/IUNetSci/botometer-python ICWSM
+import tweepy.error
 from tqdm import tqdm
 
 
@@ -24,18 +26,36 @@ def loadTotalRows(inputfilename):
     return nrows
 
 if __name__ == "__main__":
-    try:
-        args = sys.argv[1:]
-        currentToken = int(args.pop(0))
-        sampleSize = int(args.pop(0))
-        inputfilename = args.pop(0)
-        if len(args) > 0:
-            configfilename = args.pop(0)
-        else:
-            configfilename = 'TwitterAuthentication.json'
-    except Exception:
-        print ERROR + 'Please provide a valid cmd line' + RESET
+    desc = INFO
+    desc += 'TwitterMonitor tool to compute the bot scores of twitter accounts.\
+     It requires (1) an additional configuration file with Twitter OAuth \
+    credentials and (2) an external file with users data to sort them and \
+    identify super users. OBS (Botometer computes the scores)' + RESET
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('token', default='0',
+                        help='OAuth token index as provided in \
+                        TwitterMonitorConfig.json to be used for \
+                        Twitter API calls')
+    parser.add_argument('inputfilename', metavar='filename',
+                        help='Inputfile (JSON formated) with twitter user ids.')
+    parser.add_argument('-s', '--samples', default=300,
+                        metavar='value',
+                        help='Min of samples per users to be checked - \
+                        default 300.')
+    parser.add_argument('-c', '--config', default='TwitterMonitorConfig.json',
+                        metavar='filename',
+                        help='Config file (formated as JSON) - default\
+                        TwitterMonitorConfig.json.')
+    if len(sys.argv) == 1: # if no arguments, then present the help
+        parser.print_help()
         exit()
+    else:
+        args = parser.parse_args()
+
+    token = int(args.token)
+    inputfilename = args.inputfilename
+    minSamples = int(args.samples)
+    configfilename = args.config
 
     usersDefined = set()
     outputfilename = inputfilename.replace('coords.json', 'bot-scores.json')
@@ -68,7 +88,7 @@ if __name__ == "__main__":
             sample = json.loads(sample)
         except:
             invalidSamples += 1
-        if sum(sample['coords'].values()) > 300:
+        if sum(sample['coords'].values()) > minSamples:
             # print 'checking: https://twitter.com/intent/user?user_id=' + sample['userid']
             if sample['userid'] not in usersDefined:
                 userids.append(sample['userid'])
@@ -78,52 +98,46 @@ if __name__ == "__main__":
     outputfile = open(outputfilename, 'a' , 0)
     print INFO + 'Checking ' + str(len(userids)) + ' Accounts' + RESET
 
-    indexes = range(0, len(userids), sampleSize) # 171
-    indexes.append(None)
-    for i, j in zip(indexes, indexes[1:]):
-        sliceAccounts = userids[i:j]
-        try:
-            print INFO + 'Authenticating Token #' + str(currentToken) + RESET
+    tokenCounter = 0
+    for i in tqdm(range(len(userids)), desc='Botomering'):
+        uid = userids[i]
+        if tokenCounter >= 100:
+            token += 1
+            tokenCounter = 0
+            print INFO + 'Using Token #' + str(token) + RESET
+        if tokenCounter == 0:
             twitter_app_auth = {
             'consumer_key': consumer_key,
             'consumer_secret': consumer_secret,
-            'access_token': tokens[currentToken][0],
-            'access_token_secret': tokens[currentToken][1]
+            'access_token': tokens[token][0],
+            'access_token_secret': tokens[token][1]
             }
-        except IndexError:
-            print ERROR + 'No Tokens Available Anymore' + RESET
-            exit()
-        currentToken += 1
-        bom = botometer.Botometer(mashape_key=mashape_key, **twitter_app_auth)
-        print INFO + 'Fetching ' + str(len(sliceAccounts)) + ' accounts' + RESET
+            bom = botometer.Botometer(mashape_key=mashape_key, **twitter_app_auth)
         try:
-            results = list(bom.check_accounts_in(sliceAccounts))
-        except requests.exceptions.HTTPError, e:
-            print ERROR + 'Error During Requests' + RESET
-            print e
-            exit()
-        print INFO + 'Saving Scores' + RESET
-        for r in results:
-            data = r[1]
+            data = bom.check_account(uid)
             if 'user' not in data:
-                data['user'] = {'id_str':r[0]}
+                print data
+                data['user'] = {'id_str':uid}
             json.dump(data, outputfile)
             outputfile.write('\n')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        except requests.exceptions.HTTPError, e:
+            print WARNING + 'Error Requesting ' + uid + RESET
+            print ERROR + str(e) + RESET
+        except tweepy.error.TweepError, e:
+            # In case of user deleted or privacy settings changed
+            msg = str(e)
+            if 'Not authorized' in msg or 'that page does not exist' in msg:
+                data = {'user': {'id_str': uid}, 'error':msg}
+                json.dump(data, outputfile)
+                outputfile.write('\n')
+            else:
+                print ERROR + str(e) + RESET
+                print WARNING + 'https://twitter.com/intent/user?user_id=' + uid + RESET
+        except botometer.NoTimelineError, e:
+            data = {'user': {'id_str': uid}, 'error':str(e)}
+            json.dump(data, outputfile)
+            outputfile.write('\n')
+        tokenCounter += 1
 
 
 
